da0fe492720a461f551958b46f1320ccff46205f
eeshamoona <eeshamoona@gmail.com>
2021-11-06 14:00:18 -0500

    Adding instructions to User and Home Page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -3,7 +3,11 @@ import React from "react";
 function HomePage() {
   return (
     <div>
-      <h1>HELLO THIS IS THE HOME PAGE!</h1>
+      <h1>THIS IS THE HOME PAGE!</h1>
+      <p>
+        Here we can navigate to creating n order, seeing past orders, and
+        analytics(?) + user settings page
+      </p>
     </div>
   );
 }
--- src/pages/UserPage.js
+++ src/pages/UserPage.js
@@ -4,6 +4,12 @@ function UserPage() {
   return (
     <div>
       <h1>HELLO THIS IS THE USER PAGE!</h1>
+      <p>
+        We need the ability to login as a user, sign up (insert user) and delete
+        the user (they can delete their account), and sign out? We need a saved
+        state of the user so that the orders can be created under each user (FK)
+        COVERS: create + delete *Will authentication stuff get tricky?
+      </p>
     </div>
   );
 }

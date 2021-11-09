ee197604323922a47ae4fda8e6a00e795b53d9bf
eeshamoona <eeshamoona@gmail.com>
2021-11-09 16:16:07 -0600

    Create frontend for analytics

--- src/pages/AnalyticsPage.js
+++ src/pages/AnalyticsPage.js
@@ -1,23 +1,71 @@
 import React, { useEffect } from "react";
 import { getMaxPricePurchases } from "../services/api";
+import {
+  List,
+  ListItem,
+  ListItemText,
+  Card,
+  CardContent,
+  Paper,
+} from "@mui/material";
+import { makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles({
+  root: {
+    textAlignLast: "center",
+    width: "80%",
+  },
+  header: {
+    height: "50%",
+  },
+  pastButton: {
+    borderRadius: "100px",
+  },
+  list: {
+    maxHeight: "100%",
+    overflow: "auto",
+  },
+  user: {
+    borderRadius: "100px",
+  },
+});
 
 function AnalyticsPage() {
-  const [maxPricePurchases, setMaxPricePurchases] = React.useState([])
+  const classes = useStyles();
+  const [maxPricePurchases, setMaxPricePurchases] = React.useState([]);
 
   useEffect(() => {
-    getMaxPricePurchases().then((tempArray)=>{console.log(tempArray.message)});
-
-  }, [])
+    getMaxPricePurchases().then((tempArray) => {
+      console.log(tempArray.message);
+      setMaxPricePurchases(tempArray.message);
+    });
+  }, []);
 
   return (
-    <div>
-      <h1>Items Purchased At Peak Price</h1>
-      <div>
-        <p>These are some items bought by users at their peak selling price.</p>
-      </div>
-      <script>
-        
-      </script>
+    <div className={classes.root}>
+      <p>These are some items bought by users at their peak selling price.</p>
+      <Card>
+        <List>
+          <ListItem>
+            <ListItemText primary="Item Id" />
+            <ListItemText primary="Price" />
+            <ListItemText primary="User Id" />
+          </ListItem>
+        </List>
+        <CardContent>
+          <List>
+            <Paper style={{ textAlign: "-webkit-center", overflow: "auto" }}>
+              {maxPricePurchases.map((item) => (
+                <ListItem className={classes.list}>
+                  <ListItemText primary={item.item_id} />
+                  <ListItemText primary={item.price} />
+                  <ListItemText primary={item.user_id} />
+                </ListItem>
+              ))}
+            </Paper>
+          </List>
+        </CardContent>
+      </Card>
     </div>
   );
 }
